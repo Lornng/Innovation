be7d7c85e85e55c89f4abedc5d11ff4c6c5c0419
garrichf <106557702+garrichf@users.noreply.github.com>
2023-08-30 03:11:08 +1000

    Lel

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import BlockchainVisualization from './Components/BViz';
 import SearchBar from './Components/searchBar'; //Component names have to start with uppercase because if lowercase JSX treats it as a html element.
+import SpanningTable from './Components/table';
 
 function App() {
+  document.body.style.backgroundColor = "#1A2337"
   const data = {
     wallets: ['wallet1', 'wallet2', 'wallet3'],
     transactions: [
@@ -14,9 +16,9 @@ function App() {
     <div>
       <div className='App'>
         <SearchBar placeholder='Insert wallet address...'/>
+        <SpanningTable/>
       </div>
       <div>
-      <BlockchainVisualization data={data} />
       </div>
      </div>
   );
